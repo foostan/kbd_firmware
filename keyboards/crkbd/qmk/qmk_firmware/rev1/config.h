71a8d2cd57466f4d1d845a1916396bdb1a2a1cda
foostan <ks@fstn.jp>
2024-01-29 23:44:32 +0900

    Refactor crkbd

--- keyboards/crkbd/qmk/qmk_firmware/rev1/config.h
+++ keyboards/crkbd/qmk/qmk_firmware/rev1/config.h
@@ -6,6 +6,10 @@
 // #define EE_HANDS
 
 #ifdef RGB_MATRIX_ENABLE
-#    define RGB_MATRIX_LED_COUNT 54
-#    define SPLIT_TRANSPORT_MIRROR
+#  define RGB_MATRIX_LED_COUNT 54
+#  define SPLIT_TRANSPORT_MIRROR
+#endif
+
+#ifndef OLED_FONT_H
+#  define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
 #endif
--- keyboards/crkbd/qmk/qmk_firmware/rev4/halconf.h
+++ keyboards/crkbd/qmk/qmk_firmware/rev4/halconf.h
@@ -1,5 +1,3 @@
-// Copyright 2023 QMK
-// SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 
 #define HAL_USE_I2C TRUE
