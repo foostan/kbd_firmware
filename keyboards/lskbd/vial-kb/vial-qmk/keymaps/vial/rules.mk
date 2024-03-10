0ec47a32b500e9e456cb23c99cc3daef9e4081f0
foostan <ks@fstn.jp>
2024-03-10 16:14:43 +0900

    Update vial keymaps for lskbd

--- keyboards/lskbd/vial-kb/vial-qmk/keymaps/vial/rules.mk
+++ keyboards/lskbd/vial-kb/vial-qmk/keymaps/vial/rules.mk
@@ -1,6 +1,5 @@
-VIA_ENABLE  = yes
-VIAL_ENABLE = yes
-LTO_ENABLE  = yes
-
+VIA_ENABLE     = yes
+VIAL_ENABLE    = yes
+LTO_ENABLE     = yes
 VIALRGB_ENABLE = yes
-QMK_SETTINGS = no
+QMK_SETTINGS   = no
